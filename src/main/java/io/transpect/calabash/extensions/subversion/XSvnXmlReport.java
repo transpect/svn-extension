9360cb1ebff08258d9025e872e9608088b03df31
Martin Kraetke <mkraetke@gmx.de>
2020-01-29 15:55:14 +0100

    Render a SVNDirEntry ArrayList as XML

--- src/main/java/io/transpect/calabash/extensions/subversion/XSvnXmlReport.java
+++ src/main/java/io/transpect/calabash/extensions/subversion/XSvnXmlReport.java
@@ -3,6 +3,7 @@ package io.transpect.calabash.extensions.subversion;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import net.sf.saxon.s9api.QName;
@@ -17,58 +18,87 @@ import com.xmlcalabash.library.DefaultStep;
 import com.xmlcalabash.model.RuntimeValue;
 import com.xmlcalabash.runtime.XAtomicStep;
 import com.xmlcalabash.util.TreeWriter;
+
+import org.tmatesoft.svn.core.SVNDirEntry;
+
 /**
  * Returns XML-based reports or errors as result for
  * further processing in XProc pipelines
  *
  */
 public class XSvnXmlReport {
-    /**
-     * Render a HashMap as XML c:param-set
-     */
-    public XdmNode createXmlResult(HashMap<String, String> results, XProcRuntime runtime, XAtomicStep step){
-        TreeWriter tree = new TreeWriter(runtime);
-        tree.startDocument(step.getNode().getBaseURI());
-        tree.addStartElement(XProcConstants.c_param_set);
-        for(String key:results.keySet()) {
-            tree.addStartElement(XProcConstants.c_param);
-            tree.addAttribute(new QName("name"), key);
-            tree.addAttribute(new QName("value"), results.get(key));
-            tree.addEndElement();
-        }
-        tree.addEndElement();
-        tree.endDocument();
-        return tree.getResult();
+  /**
+   * Render a HashMap as XML (c:param-set)
+   */
+  public XdmNode createXmlResult(HashMap<String, String> results, XProcRuntime runtime, XAtomicStep step){
+    TreeWriter tree = new TreeWriter(runtime);
+    tree.startDocument(step.getNode().getBaseURI());
+    tree.addStartElement(XProcConstants.c_param_set);
+    for(String key:results.keySet()) {
+      tree.addStartElement(XProcConstants.c_param);
+      tree.addAttribute(new QName("name"), key);
+      tree.addAttribute(new QName("value"), results.get(key));
+      tree.addEndElement();
     }
-    public XdmNode createXmlResult(String baseURI, String type, String[] results, XProcRuntime runtime, XAtomicStep step){
-        TreeWriter tree = new TreeWriter(runtime);
-        tree.startDocument(step.getNode().getBaseURI());
-        tree.addStartElement(XProcConstants.c_param_set);
-        tree.addAttribute(new QName("xml", "http://www.w3.org/XML/1998/namespace", "base"), baseURI);
-        for(int i = 0; i < results.length; i++){
-            tree.addStartElement(XProcConstants.c_param);
-            tree.addAttribute(new QName("name"), type);
-            tree.addAttribute(new QName("value"), results[i]);
-            tree.addEndElement();
-        }
-        tree.addEndElement();
-        tree.endDocument();
-        return tree.getResult();
+    tree.addEndElement();
+    tree.endDocument();
+    return tree.getResult();
+  }
+  /**
+   * Render a String array as XML (c:param-set)
+   */
+  public XdmNode createXmlResult(String baseURI, String type, String[] results, XProcRuntime runtime, XAtomicStep step){
+    TreeWriter tree = new TreeWriter(runtime);
+    tree.startDocument(step.getNode().getBaseURI());
+    tree.addStartElement(XProcConstants.c_param_set);
+    tree.addAttribute(new QName("xml", "http://www.w3.org/XML/1998/namespace", "base"), baseURI);
+    for(int i = 0; i < results.length; i++){
+      tree.addStartElement(XProcConstants.c_param);
+      tree.addAttribute(new QName("name"), type);
+      tree.addAttribute(new QName("value"), results[i]);
+      tree.addEndElement();
     }
-    /**
-     * Render errors as XML c:errors
-     */
-    public XdmNode createXmlError(String message, XProcRuntime runtime, XAtomicStep step){
-        TreeWriter tree = new TreeWriter(runtime);
-        tree.startDocument(step.getNode().getBaseURI());
-        tree.addStartElement(XProcConstants.c_errors);
-        tree.addAttribute(new QName("code"), "svn-error");
-        tree.addStartElement(XProcConstants.c_error);
-        tree.addAttribute(new QName("code"), "error");
-        tree.addText(message);
-        tree.addEndElement();
-        tree.addEndElement();
-        tree.endDocument();
-        return tree.getResult();
+    tree.addEndElement();
+    tree.endDocument();
+    return tree.getResult();
+  }
+  /**
+   * Render a SVNDirEntry ArrayList as XML (c:files)
+   */
+  public XdmNode createXmlResult(String baseURI, ArrayList<SVNDirEntry> results, XProcRuntime runtime, XAtomicStep step){
+    TreeWriter tree = new TreeWriter(runtime);
+    tree.startDocument(step.getNode().getBaseURI());
+    tree.addStartElement(new QName("c", "http://www.w3.org/ns/xproc-step", "files"));
+    tree.addAttribute(new QName("xml", "http://www.w3.org/XML/1998/namespace", "base"), baseURI);
+    for(int i = 0; i < results.size(); i++){
+      String type = results.get(i).getKind().toString() == "dir" ? "directory" : "file";
+      tree.addStartElement(new QName("c", "http://www.w3.org/ns/xproc-step", type));
+      tree.addAttribute(new QName("name"), results.get(i).getName());
+      tree.addAttribute(new QName("author"), results.get(i).getAuthor());
+      tree.addAttribute(new QName("date"), results.get(i).getDate().toString());
+      tree.addAttribute(new QName("revision"), String.valueOf(results.get(i).getRevision()));
+      tree.addAttribute(new QName("size"), String.valueOf(results.get(i).getSize()));
+      tree.addAttribute(new QName("msg"), results.get(i).getCommitMessage());
+      tree.addEndElement();
     }
+    tree.addEndElement();
+    tree.endDocument();
+    return tree.getResult();
+  }
+  /**
+   * Render errors as XML c:errors
+   */
+  public XdmNode createXmlError(String message, XProcRuntime runtime, XAtomicStep step){
+    TreeWriter tree = new TreeWriter(runtime);
+    tree.startDocument(step.getNode().getBaseURI());
+    tree.addStartElement(XProcConstants.c_errors);
+    tree.addAttribute(new QName("code"), "svn-error");
+    tree.addStartElement(XProcConstants.c_error);
+    tree.addAttribute(new QName("code"), "error");
+    tree.addText(message);
+    tree.addEndElement();
+    tree.addEndElement();
+    tree.endDocument();
+    return tree.getResult();
+  }
 }
