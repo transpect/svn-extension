1e0b9d903c580302f232ca518f72478ad7c9bc52
Martin Kraetke <mkraetke@gmx.de>
2020-01-29 20:56:34 +0100

    add svn list src

---
+++ src/main/java/io/transpect/calabash/extensions/subversion/XSvnList.java
@@ -0,0 +1,98 @@
+package io.transpect.calabash.extensions.subversion;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import java.io.File;
+
+import net.sf.saxon.s9api.QName;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmNode;
+
+import com.xmlcalabash.core.XMLCalabash;
+import com.xmlcalabash.core.XProcRuntime;
+import com.xmlcalabash.io.WritablePipe;
+import com.xmlcalabash.library.DefaultStep;
+import com.xmlcalabash.model.RuntimeValue;
+import com.xmlcalabash.runtime.XAtomicStep;
+import com.xmlcalabash.util.TreeWriter;
+
+import org.tmatesoft.svn.core.SVNNodeKind;
+import org.tmatesoft.svn.core.SVNDirEntry;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.io.SVNRepository;
+
+import io.transpect.calabash.extensions.subversion.XSvnConnect;
+import io.transpect.calabash.extensions.subversion.XSvnXmlReport;
+/**
+ * Performs svn ls as XML Calabash extension step for
+ * XProc. The class connects to a Subversion repository and
+ * provides the results as XML document.
+ *
+ * @see XSvnConnect
+ */
+public class XSvnList extends DefaultStep {
+    private WritablePipe result = null;
+
+    public XSvnList(XProcRuntime runtime, XAtomicStep step) {
+        super(runtime,step);
+    }
+    @Override
+    public void setOutput(String port, WritablePipe pipe) {
+        result = pipe;
+    }
+    @Override
+    public void reset() {
+        result.resetWriter();
+    }
+    @Override
+    public void run() throws SaxonApiException {
+        super.run();
+	String url = getOption(new QName("repo")).getString();
+        String username = getOption(new QName("username")).getString();
+        String password = getOption(new QName("password")).getString();
+        Boolean recursive = getOption(new QName("recursive")).getString() == "yes" ? true : false;
+        XSvnXmlReport report = new XSvnXmlReport();
+	try{
+          XSvnConnect connection = new XSvnConnect(url, username, password);
+          SVNRepository repository = connection.getRepository();
+          XdmNode xmlResult = createXmlDirTree(repository, "", runtime, step, recursive);
+          result.write(xmlResult);
+	}catch(SVNException svne){
+	    System.out.println(svne.getMessage());
+            XdmNode xmlError = report.createXmlError(svne.getMessage(), runtime, step);
+	    result.write(xmlError);
+	}
+    }
+
+  public static XdmNode createXmlDirTree(SVNRepository repository, String path, XProcRuntime runtime, XAtomicStep step, Boolean recursive) throws SVNException {
+    TreeWriter tree = new TreeWriter(runtime);
+    tree.startDocument(step.getNode().getBaseURI());
+    tree.addStartElement(new QName("c", "http://www.w3.org/ns/xproc-step", "files"));
+    tree.addAttribute(new QName("xml", "http://www.w3.org/XML/1998/namespace", "base"), repository.getLocation().toString());
+    listEntries(repository, "", runtime, step, tree, recursive);
+    tree.addEndElement();
+    tree.endDocument();
+    return tree.getResult();
+  }
+  
+  public static TreeWriter listEntries(SVNRepository repository, String path, XProcRuntime runtime, XAtomicStep step, TreeWriter tree, Boolean recursive) throws SVNException {
+    Collection entries = repository.getDir( path, -1 , null , (Collection) null );
+    Iterator iterator = entries.iterator( );
+    while ( iterator.hasNext( ) ) {
+      SVNDirEntry entry = (SVNDirEntry) iterator.next( );
+      String elementName = entry.getKind() == SVNNodeKind.DIR ? "directory" : "file";
+      tree.addStartElement(new QName("c", "http://www.w3.org/ns/xproc-step", elementName));
+      tree.addAttribute(new QName("name"), entry.getName());
+      tree.addAttribute(new QName("author"), entry.getAuthor());
+      tree.addAttribute(new QName("date"), entry.getDate().toString());
+      tree.addAttribute(new QName("revision"), String.valueOf(entry.getRevision()));
+      tree.addAttribute(new QName("size"), String.valueOf(entry.getSize()));
+      if ( entry.getKind() == SVNNodeKind.DIR && recursive == true ) {
+        listEntries(repository, (path.equals( "" )) ? entry.getName( ) : path + "/" + entry.getName( ), runtime, step, tree, recursive);
+      }
+      tree.addEndElement();
+    }
+    return tree;
+  }
+}
